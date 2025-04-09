59411550c5b602a9868504ab158c07bbebb0f338
Elhama Tokhi <tokhielaham30@gmail.com>
2025-04-09 10:13:00 +0200

    Displayed users' data back to the user

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
 const app = express();
 const PORT = 3000;
 
@@ -10,14 +9,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // D:\CTI-Practiec ==> __dirname gives us the directory of a file
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.post("/users", (req, res) => {
-  console.log(req.body);
+  const { fname, message } = req.body;
+  res.send(`
+    <h1>Submitted Data</h1>
+    <p><b>Name</b>: ${fname}</p>
+     <p><b>Message</b>: ${message}</p>
+    `);
 });
 app.listen(PORT, () => {
   console.log(`The server is listening on port:${PORT}`);
