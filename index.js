3a5247d0ede6276c123c5d40f89bd97c05af97f8
Elhama Tokhi <tokhielhama30@gmail.com>
2025-07-29 13:04:40 +0200

    Initialize Express app with PostgreSQL pool, middleware, and fetch districts from DB

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Pool } from "pg";
+
+/**
+ *
+ * Pool in pg is a connection manager that efficiently handles
+ * multiple PostgreSQL database connections by reusing them, improving performance and scalability.
+ *
+ * The methods of a Pool let you run SQL queries, get a client for advanced control,
+ * close all connections, and listen to connection-related events.
+ * **/
+const app = express();
+const PORT = 3000;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "Afghanistan",
+  password: "Sahil@456",
+  port: 5432,
+}); // you write the configuration of your DB here
+
+// __dirname Setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middlware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+/*------------------Here we start the app----------------- */
+let userScore = 0;
+let cities = [];
+let hintRegion = undefined;
+let hintDistrict = "";
+
+pool.query('SELECT * FROM "Districts"', (error, result) => {
+  if (error) {
+    console.log("Error while fetching cities", error);
+  } else {
+    cities = result.rows;
+    console.log(cities.length);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is listening...");
+});
