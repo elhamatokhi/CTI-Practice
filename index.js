0bd1853257e3f6360e2b151b4e1837c99f6fa420
Elhama Tokhi <tokhielhama30@gmail.com>
2025-07-29 16:30:19 +0200

    Configured /check route for check button

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const __dirname = path.dirname(__filename);
 
 // Middlware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 /*------------------Here we start the app----------------- */
@@ -43,7 +43,61 @@ pool.query('SELECT * FROM "Districts"', (error, result) => {
     console.log("Error while fetching cities", error);
   } else {
     cities = result.rows;
-    console.log(cities.length);
+  }
+});
+
+//  Routes
+
+app.get("/", (req, res) => {
+  if (cities.length > 0) {
+    let randomDistrict;
+
+    if (hintRegion) {
+      randomDistrict = hintDistrict;
+    } else {
+      const randomIndex = Math.floor(Math.random() * cities.length);
+      randomDistrict = cities[randomIndex].district;
+    }
+    res.render("index", { userScore, randomDistrict, hintRegion });
+  } else {
+    res.send(`شهری یافت نشد.`);
+  }
+});
+
+app.post("/check", (req, res) => {
+  const action = req.body.action;
+  const userInput = req.body.cityInput;
+
+  const district = req.body.district;
+  if (action === "check") {
+    pool.query(
+      'SELECT province FROM "Districts" WHERE district = $1',
+      [district],
+      (err, result) => {
+        if (err) {
+          console.error("Something went wrong during executing query", err);
+          res.status(500).send("خطا X!");
+        } else {
+          if (result.rows.length > 0) {
+            const province = result.rows[0].province;
+            hintRegion = undefined;
+            if (userInput === province) {
+              userScore++;
+              res.redirect("/");
+            } else {
+              userScore--;
+              if (userScore < 0) {
+                res.redirect("/gameover");
+              } else {
+                res.redirect("/");
+              }
+            }
+          }
+        }
+      }
+    );
+  }
+  if (action === "hint") {
   }
 });
 
